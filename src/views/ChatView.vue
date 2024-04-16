54dc0dbb2d3f6f1ee269d9e4ad5b2a228896e8a8
RichardKobay <ricardo.roel110@gmail.com>
2024-04-15 21:11:49 -0600

    first version finished

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,67 +1,75 @@
 <script setup>
 import {onMounted, ref} from 'vue';
+import {useRoute} from "vue-router";
 import axios from "axios";
 
 const messages = ref([]);
-const newMessage = ref('');
-const requestStr = ref({});
-const response = ref();
+const userResponses = ref([]);
 
 onMounted(() => {
+  const route = useRoute();
+  const queryResponses = route.query.userResponses
+  if (queryResponses)
+    userResponses.value = JSON.parse(queryResponses)
 
+  const msg = {
+    role: "user",
+    content: "A partir de ahora eres un profesor de POO en lenguaje de programación de java. QUEDA ESTRICTAMENTE PROHIBIDO HABLAR DE TEMAS QUE NO TENGAN QUE VER CON PROGRAMACIÓN ORIENTADA A OBJETOS. TE PUEDES SALIR PARA EXPLICAR TEMAS DE PROGRAMACIÓN, PERO FUERA DE LA PROGRAMACIÓN QUEDA PROHIBIDO HABLAR"
+  }
+  const msg2 = {
+    role: "user",
+    content: "Tienes este JSON con las preguntas y las respuestas de un exámen de POO. Tomando en cuenta las respuestas, crea un temario con los temas que faltan por reforzar: " + JSON.stringify(userResponses.value)
+  }
+  messages.value.push(msg)
+  messages.value.push(msg2)
+
+  sendReq()
 })
 
-const initRequestObj = () => {
-  requestStr.value.model = "gpt-3.5-turbo"
-  requestStr.value.messages = messages.value;
-}
-
-const sendMessage = () => {
-  if (newMessage.value.trim() !== '') {
-    messages.value.push({ role: "user", content: newMessage.value });
-
-
-
-    setTimeout(receiveMessage, 1000);
-    newMessage.value = '';
-  }
-}
-
-const receiveMessage = () => {
-  messages.value.push({ role: 'profesor', content: 'received' });
-  requestStr.value.messages = messages.value;
-
-  console.log(JSON.stringify(requestStr.value))
-}
-
-const getChatGPTResponse = async()=> {
+const sendReq = async () => {
   try {
-    const response = await axios.get("http://localhost:8080/chat")
-    preguntasArray.value = JSON.parse(preguntasString.value.response);
-    generandoExamen.value = false;
-  } catch (error) {
-    console.error("Error al obtener las preguntas" + error);
-    errorGenerarExamen.value = true;
-  }
+    const model = "gpt-3.5-turbo";
+    const finalReq = {
+      "model": model,
+      "messages": messages.value
+    };
 
-  return{
-    preguntas: preguntasString,
-    preguntasArray: preguntasArray
+    const otherFinalReq = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {
+          "role": "system",
+          "content": "You are a helpful assistant."
+        },
+        {
+          "role": "user",
+          "content": "Hello!"
+        }
+      ]
+    }
+
+
+    const response = await axios.post('http://localhost:8080/request', {
+      prompt: JSON.stringify(finalReq)
+    });
+
+    console.log(response.data);
+    messages.value.push({"role": "professor", "content": response.data});
+  } catch (error) {
+    console.error('Error sending request:', error);
   }
-}
+};
+
 </script>
 
 <template>
   <div>
     <h2 class="text-green-500 text-5xl">Chat</h2>
     <div class="border-green-300 border-2">
-      <div v-for="(message, index) in messages" :key="index" :class="{'bg-green-200': message.role === 'profesor', 'bg-green-300': message.role === 'user'}" class="">
-        {{ message.content }}
+      <div v-for="(message, index) in messages" :key="index"
+           :class="{'bg-green-200': message.role === 'profesor', 'bg-green-300': message.role === 'user'}" class="">
+        <p v-if="message.role === 'professor'">{{ message.content.response }}</p>
       </div>
-    </div>
-    <div class="border-2 border-green-300 flex">
-      <input v-model="newMessage" type="text" placeholder="Escribe un mensaje..." class="flex-grow">
-      <button @click="sendMessage">Enviar</button>
     </div>
   </div>
 </template>
--- src/views/ExamView.vue
+++ src/views/ExamView.vue
@@ -1,57 +1,340 @@
 <script setup>
-import axios from "axios";
-import {ref, onMounted} from "vue";
+import { ref } from "vue";
+import router from "@/router/index.js";
 
-onMounted(() => {
-  getPreguntas()
+const props = defineProps({
+  userResponses: Array
 })
 
-const preguntasString = ref();
-const preguntasArray = ref([]);
-const respuestas = ref(Array(preguntasArray.value.length).fill(''));
-const generandoExamen = ref(true);
-const errorGenerarExamen = ref(false);
+const exam = ref([
+  {
+    "pregunta": "¿Qué es una clase en poo?",
+    "respuestas": [
+      "Un prototipo de objeto",
+      "Una instancia de objeto",
+      "Un método",
+      "Un valor primitivo"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es un objeto en poo?",
+    "respuestas": [
+      "Un prototipo de clase",
+      "Una instancia de clase",
+      "Un tipo de dato primitivo",
+      "Un conjunto de método"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es la abstracción en POO?",
+    "respuestas": [
+      "Ocultar la implementación interna de un objeto",
+      "Convertir un objeto en una instancia de clase",
+      "Representar objetos del mundo real",
+      "Acceder a los atributos públicos de una clase"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es el encapsulamiento en POO?",
+    "respuestas": [
+      "Ocultar la implementación interna de un objeto",
+      "Exponer todos los métodos de una clase",
+      "Acceder directamente a los atributos privados de una clase",
+      "Hacer públicos todos los atributos de una clase"
+    ]
+  },
+  {
+    "pregunta": "Qué es el polimorfismo en POO?",
+    "respuestas": [
+      "La capacidad de una clase para heredar de múltiples clases",
+      "La capacidad de una clase para tener múltiples constructores",
+      "La capacidad de una clase para tener múltiples formas",
+      "La capacidad de una clase para tener múltiples instancias"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es la herencia en POO?",
+    "respuestas": [
+      "La capacidad de una clase para tener múltiples formas",
+      "La capacidad de una clase para tener múltiples instancias",
+      "La capacidad de una clase para heredar atributos y métodos de otra clase",
+      "La capacidad de una clase para implementar múltiples interfaces"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es la herencia jerárquica en POO?",
+    "respuestas": [
+      "Una clase que hereda de múltiples clases",
+      "Una clase que es heredada por múltiples clases",
+      "Una cadena de clases que se heredan unas de otras",
+      "Una clase que no tiene clases que hereden de ella"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es el casting en POO?",
+    "respuestas": [
+      "Convertir un objeto de una clase a otra clase relacionada",
+      "Ocultar la implementación interna de un objeto",
+      "Hacer que un objeto sea una instancia de su clase base",
+      "Acceder a los atributos privados de una clase"
+    ]
+  },
+  {
+    "pregunta": "¿Cuáles son los tipos de polimorfismo en POO?",
+    "respuestas": [
+      "Estático y dinámico",
+      "Público y privado",
+      "Simple y compuesto",
+      "Fuerte y débil"
+    ]
+  },
+  {
+    "pregunta": "¿Qué son las clases anidadas en POO?",
+    "respuestas": [
+      "Clases que tienen múltiples constructores",
+      "Clases que se definen dentro de otras clases",
+      "Clases que no pueden tener objetos",
+      "Clases que no pueden tener métodos"
+    ]
+  },
+  {
+    "pregunta": "¿Qué es una interfaz en POO?",
+    "respuestas": [
+      "Una clase que no puede ser instanciada",
+      "Una clase abstracta",
+      "Una especificación de métodos que una clase debe implementar",
+      "Una clase que no puede heredar de otras clases"
+    ]
+  },
+  {
+    "pregunta": "¿Qué son las interfaces anidadas en POO?",
+    "respuestas": [
+      "Interfaces que están dentro de otras interfaces",
+      "Interfaces que no pueden tener métodos",
+      "Interfaces que no pueden tener clases que las implementen",
+      "Interfaces que solo pueden ser implementadas por clases abstractas"
+    ]
+  },
+  {
+    "pregunta": "¿Qué son los genéricos en POO?",
+    "respuestas": [
+      "Tipos de datos primitivos",
+      "Tipos de datos que pueden ser utilizados en cualquier parte del código",
+      "Tipos de datos que pueden ser utilizados en cualquier parte del código, pero no en clases",
+      "Tipos de datos parametrizados que permiten reutilizar código con diferentes tipos de datos"
+    ]
+  },
+  {
+    "pregunta": "¿Cuál de las siguientes NO es una característica de la POO?",
+    "respuestas": [
+      "Encapsulamiento",
+      "Herencia",
+      "Iteración",
+      "Polimorfismo"
+    ]
+  },
+  {
+    "pregunta": "¿Cuál de las siguientes afirmaciones sobre las clases y objetos es FALSA?",
+    "respuestas": [
+      "Una clase define las propiedades y comportamientos comunes de los objetos",
+      "Un objeto es una instancia concreta de una clase",
+      "Una clase no puede tener múltiples objetos",
+      "Los objetos pueden interactuar entre sí"
+    ]
+  },
 
-const getPreguntas = async()=> {
-  try {
-    const response = await axios.get("http://localhost:8080/get-test")
-    preguntasString.value = response.data;
-    preguntasArray.value = JSON.parse(preguntasString.value.response);
-    generandoExamen.value = false;
-  } catch (error) {
-    console.error("Error al obtener las preguntas" + error);
-    errorGenerarExamen.value = true;
+  {
+    "pregunta": "¿Cuál de las siguientes afirmaciones describe mejor la herencia?",
+    "respuestas": [
+      "Una clase que hereda de otra clase adquiere sus atributos y comportamientos.",
+      "Una clase que hereda de otra clase anula todos sus métodos.",
+      "Una clase que hereda de otra clase no puede acceder a sus métodos.",
+      "Una clase que hereda de otra clase no puede tener sus propios métodos."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Cuál de las siguientes opciones describe el polimorfismo?",
+    "respuestas": [
+      "La capacidad de una clase para tener múltiples formas.",
+      "La capacidad de una clase para heredar de múltiples clases.",
+      "La capacidad de una clase para tener múltiples instancias.",
+      "La capacidad de una clase para tener múltiples atributos."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Qué es una clase abstracta?",
+    "respuestas": [
+      "Una clase que no puede tener objetos.",
+      "Una clase que solo puede heredar de una clase concreta.",
+      "Una clase que no puede ser heredada.",
+      "Una clase que puede contener métodos abstractos."
+    ]
+  },
+
+  {
+    "pregunta": "¿Cuál de las siguientes opciones describe mejor el encapsulamiento?",
+    "respuestas": [
+      "Ocultar la implementación interna de un objeto.",
+      "Exponer todos los métodos de una clase.",
+      "Hacer públicos todos los atributos de una clase.",
+      "Acceder directamente a los atributos privados de una clase."
+    ]
+  },
+
+  {
+    "pregunta": "¿Qué es un método abstracto?",
+    "respuestas": [
+      "Un método que no tiene implementación.",
+      "Un método que no puede ser llamado.",
+      " Un método que solo puede ser llamado por otras clases.",
+      "Un método que no puede ser heredado."
+    ]
+  },
+
+  {
+    "pregunta": "¿Cuál de las siguientes opciones describe mejor una interfaz?",
+    "respuestas": [
+      "Una especificación de métodos que una clase debe implementar.",
+      "Una clase concreta que puede tener instancias.",
+      "Una clase que no puede tener métodos.",
+      "Una clase que no puede ser instanciada."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Cuál de las siguientes afirmaciones describe mejor el casting?",
+    "respuestas": [
+      "Convertir un objeto de una clase a otra clase relacionada.",
+      "Ocultar la implementación interna de un objeto.",
+      "Acceder a los atributos privados de una clase.",
+      "Hacer que un objeto sea una instancia de su clase base."
+    ]
+  },
+
+
+  {
+    "pregunta": "Qué es la herencia múltiple?",
+    "respuestas": [
+      "Una clase que hereda de múltiples clases.",
+      "Una clase que hereda de una clase abstracta.",
+      "Una clase que tiene múltiples constructores.",
+      " Una clase que no puede tener objetos."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Qué es el polimorfismo estático?",
+    "respuestas": [
+      "Resolución del método en tiempo de ejecución.",
+      "Resolución del método en tiempo de compilación.",
+      "La capacidad de una clase para tener múltiples formas.",
+      "La capacidad de una clase para heredar de múltiples clases."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Qué es el polimorfismo dinámico?",
+    "respuestas": [
+      "Resolución del método en tiempo de compilación.",
+      "Resolución del método en tiempo de ejecución.",
+      "La capacidad de una clase para tener múltiples formas.",
+      "La capacidad de una clase para heredar de múltiples clases."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Qué es la herencia simple?",
+    "respuestas": [
+      "Una clase que hereda de una sola clase.",
+      "Una clase que hereda de múltiples clases.",
+      "Una clase que hereda solo de interfaces.",
+      "Una clase que no puede heredar de otras clases."
+    ]
+  },
+
+
+  {
+    "pregunta": "¿Cuál de las siguientes afirmaciones sobre clases anidadas es VERDADERA?",
+    "respuestas": [
+      "Las clases anidadas no pueden tener atributos.",
+      "Las clases anidadas no pueden tener métodos.",
+      "Las clases anidadas pueden acceder a los miembros privados de la clase externa.",
+      "Las clases anidadas no pueden tener constructores."
+    ]
+  },
+
+  {
+    "pregunta": "¿Qué es una interfaz anidada?",
+    "respuestas": [
+      "Una interfaz dentro de otra interfaz.",
+      "Una interfaz que no puede tener métodos.",
+      "Una interfaz que no puede ser implementada por clases concretas.",
+      "Una interfaz que solo puede ser implementada por clases abstractas."
+    ]
+  },
+  {
+    "pregunta": "¿Cuál es el propósito principal de los genéricos en POO?",
+    "respuestas": [
+      "Permitir la reutilización de código con diferentes tipos de datos.",
+      "Ocultar la implementación interna de un objeto.",
+      "Facilitar la herencia múltiple.",
+      "Convertir un objeto de una clase a otra clase relacionada."
+    ]
+  },
+  {
+    "pregunta": "¿Cuál de las siguientes opciones describe mejor el concepto de composición en POO?",
+    "respuestas": [
+      "La capacidad de una clase para tener múltiples instancias.",
+      "La capacidad de una clase para tener múltiples formas.",
+      "La creación de objetos complejos mediante la combinación de otros objetos.",
+      "La capacidad de una clase para tener múltiples   constructores."
+    ]
+  }
+]);
+const selectedAnswers = ref([]);
+const userResponses = ref(props.userResponses || []);
+
+
+const submitForm = () => {
+
+  if (selectedAnswers.value.length !== exam.value.length) {
+    alert("Por favor, responde todas las preguntas antes de enviar el formulario.");
+    return;
   }
 
-  return{
-    preguntas: preguntasString,
-    preguntasArray: preguntasArray
+  for (let i = 0; i < exam.value.length; i++) {
+    userResponses.value.push({
+      pregunta: exam.value[i].pregunta,
+      respuesta: selectedAnswers.value[i]
+    });
   }
+  router.push({ path: '/chat', query: { userResponses: JSON.stringify(userResponses.value) } })
 }
-
-const submitExamen = () => {
-  console.log('Respuestas del usuario:', respuestas.value);
-};
 </script>
 
 <template>
   <div>
     <h2 class="text-green-500 text-5xl">Examen</h2>
 
-    <p v-if="generandoExamen">Generando examen...</p>
-    <form v-else @submit.prevent="submitExamen">
-      <div v-for="(pregunta, index) in preguntasArray" :key="index">
-        <h3>{{ pregunta['pregunta_' + (index + 1)] }}</h3>
-        <div v-for="(respuesta, i) in pregunta.respuestas" :key="i">
-          <label>
-            <input type="radio" :name="'respuesta_' + index" :value="respuesta" v-model="respuestas[index]">
-            {{ respuesta }}
-          </label>
+    <form @submit.prevent="submitForm">
+      <div v-for="(pregunta, index) in exam" :key="index">
+        <p>{{ index + 1 }}. {{ pregunta.pregunta }}</p>
+        <div v-for="(respuesta, rIndex) in pregunta.respuestas" :key="rIndex">
+          <input type="radio" :id="'respuesta_' + index + '_' + rIndex" :value="respuesta" v-model="selectedAnswers[index]" required>
+          <label :for="'respuesta_' + index + '_' + rIndex">{{ respuesta }}</label>
         </div>
+        <hr>
       </div>
-      <button type="submit">Enviar examen</button>
+      <button type="submit">Enviar respuestas</button>
     </form>
 
-    <h2 v-if="errorGenerarExamen">Error al generar el exámen, favor de actualizar la página</h2>
   </div>
 </template>
